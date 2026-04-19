2072444454a4d6d7471c7e5d11a0e3b55c93f481
Matheus Felipe <matheus.maarfe@hotmail.com>
2026-04-19 11:00:18 -0300

    feat: add savegnago real offer collector using vtex graphql search

--- app/collectors/__init__.py
+++ app/collectors/__init__.py
@@ -3,9 +3,11 @@
 from app.collectors.base_collector import BaseCollector
 from app.collectors.json_market_collector import JsonMarketCollector
 from app.collectors.market_data_loader import MarketDataLoader
+from app.collectors.savegnago_offer_collector import SavegnagoOfferCollector
 
 __all__ = [
     "BaseCollector",
     "JsonMarketCollector",
     "MarketDataLoader",
+    "SavegnagoOfferCollector",
 ]
